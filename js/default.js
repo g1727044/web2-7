560b194e7db9e0690274b0b10a9fa32931467601
g1727044 <32856056+g1727044@users.noreply.github.com>
2018-11-12 17:48:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,15 +3,15 @@ var number=Math.floor(Math.random()*3);
 var answer=parseInt(window.prompt('数当てゲーム。0~2の数字を入力してください！'));
 
 var message;
-if(answer===number){
-message='あたり！';
-}else if(answer<number){
-message='残念！もっと大きいです';
-}else if(answer>number){
-message='残念！もっと小さいです';
+if(answer === number){
+  message = 'あたり！';
+}else if(answer < number){
+  message = '残念！もっと大きいです';
+}else if(answer > number){
+  message = '残念！もっと小さいです';
 }else{
-message='0~2の数字を入力してください';
+  message = '0~2の数字を入力してください';
 }
 
 // 結果を表示
-document.getElementById('choice'),textContent=message;
+document.getElementById('choice'),textContent = message;
